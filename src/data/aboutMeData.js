2a560e7349b6f23aa330b10dcb9537e20c954bb7
Eyu <eyobedmes2@gmail.com>
2023-12-03 01:36:30 +0300

    first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: 'I am a fourth-year Software Engineering student at Addis Abeba Science and Technology	University with a strong interest in technology and Problem-solving.',
+	},
+	{
+		id: 2,
+		bio: 'SKILLS - Proficient in languages such as HTML5, CSS3, JavaScript, and Reactjs. worked on CMS tools like wordpress to develop portfolio website, Good at designing interactive user interfaces using figma tool , Computer skills such as Word, excel, and PowerPoint- generally proficient in using office 365, Have experience in software quality end to end(E2E) testing using cypress, Have ability to collaborate and work with team also I have worked on many projects where I had to collaborate with different people.',
+	},
+];
